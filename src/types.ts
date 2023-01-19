52fa29db601037e4d11f5874a3a01a161b46bc1e
nzambello <nzambello@protonmail.com>
2023-01-19 18:57:04 +0100

    refactor: add memoryID in chat log lines

--- src/types.ts
+++ src/types.ts
@@ -576,6 +576,10 @@ export type ChatLogLine = {
    */
   media?: ChatMedium[];
   /**
+   * ID of the Memory object referenced in this line.
+   */
+  memoryID?: string;
+  /**
    * Dialog State Machine context variables after the emission, if present. Empty if the line is inbound.
    */
   contextVars?: { [key: string]: string };
