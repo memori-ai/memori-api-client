755108eceb9b7af274efc60e39562237f42c8f5f
nzambello <nzambello@protonmail.com>
2023-04-14 17:01:19 +0200

    test: temp fix for search api url, will revert to prod

--- src/engine/search.test.ts
+++ src/engine/search.test.ts
@@ -1,17 +1,18 @@
 import memori from '../index';
 
-const client = memori('https://backend.memori.ai');
+// FIXME: switch back to production when backend fixes request processing issue
+const client = memori('https://backend-staging.memori.ai');
 
 describe('engine/search api', () => {
   it('works on search apis', async () => {
     expect(
-      await client.search.postRandom('768b9654-e781-4c3c-81fa-ae1529d1bfbe')
+      await client.search.searchMemory('768b9654-e781-4c3c-81fa-ae1529d1bfbe')
     ).not.toBeNull();
   });
 
   it('works on search apis with shorthand version', async () => {
     expect(
-      await client.postRandom('768b9654-e781-4c3c-81fa-ae1529d1bfbe')
+      await client.searchMemory('768b9654-e781-4c3c-81fa-ae1529d1bfbe')
     ).not.toBeNull();
   });
 });
